0bcfbf11b9202b207a4136d6ed1bbf5e6990287c
Jesse Brown <Brownjesse8895@yahoo.com>
2021-05-24 12:55:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function multiply(x,y){
-    const num1 = x
-    const num2 = y
-    num1 * num2
+const num1 = 31;
+const num2 = 2;
 
-}
-multiply(8,8)
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+
+let max = (1,2,4,15,20)
+
+max.math.max()
 // Write your code here
